66f1a95db25c2b313bd80f22fb25de8a1aaa2207
Danielpinerosmunoz <daniel123pineros@gmail.com>
2023-06-15 16:28:37 -0500

    Rutas en proceso

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from "./Pages/Home"
+import Home from "./Pages/Enfermedad"
+import Home from "./Pages/MyAccount"
+import Home from "./Pages/Sign in"
+import { useRoutes } from 'react-router-dom'
+
+const AppRoutes = () => {
+  let routes = useRoutes ([
+    {path: "/Home", element: <Home/>},
+    {path: "/Enfermedad", element: <Enfermedad/>},
+    {path: "/MyAccount", element: <MyAccount/>},
+    {path: "/Sign in", element: <Sign in/>},
+
+
+
+
+  ])
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
    ,<div>
